aa2b2bdfed063bf4650f07cebebd256494e3078d
Cimeto <84658014+Cimeto@users.noreply.github.com>
2021-05-23 15:32:54 +0200

    Added ListExtension.cs

---
+++ Scripts/Extensions/ListExtension.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+public static class ListExtension
+{
+    private static System.Random rng = new System.Random();
+
+    public static T Random<T>(this List<T> list)
+    {
+        if (list.Count == 0) return default;
+        return list[UnityEngine.Random.Range(0, list.Count)];
+    }
+
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+}
