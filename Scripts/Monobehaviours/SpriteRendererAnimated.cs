f9d89cf3861a15977dbc795c4ddc749173e3fb0a
Cimeto <84658014+Cimeto@users.noreply.github.com>
2021-05-23 14:36:20 +0200

    Added SpriteRendererAnimated
    This script is very simple, but allows to make a SpriteRenderer to cycle between different sprites very easily

---
+++ Scripts/Monobehaviours/SpriteRendererAnimated.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Simple Sprite Renderer animator. Set the sprites list and the speed to cycle between them.
+/// </summary>
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteRendererAnimated : MonoBehaviour
+{
+    public List<Sprite> Sprites;
+    public float Speed;
+    public bool Loop;
+
+    private float timer;
+    private int spriteIndex;
+
+    private void Update()
+    {
+        if (!Loop && spriteIndex == Sprites.Count) return;
+        if (Loop && spriteIndex == Sprites.Count) spriteIndex = 0;
+
+        timer += Time.deltaTime;
+        if (timer > (1 / Speed))
+        {
+            timer = 0;
+            GetComponent<SpriteRenderer>().sprite = Sprites[spriteIndex];
+            spriteIndex += 1;
+        }
+    }
+}
