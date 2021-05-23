b01c887d7a2bfee790ffc75319fa5b2c5c97720e
Cimeto <84658014+Cimeto@users.noreply.github.com>
2021-05-23 16:17:17 +0200

    Create SpriteRendererBlinking.cs

---
+++ Scripts/Monobehaviours/SpriteRendererBlinking.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Very simple script that makes a SpriteRenderer to blink continuously
+/// </summary>
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteRendererBlinking : MonoBehaviour
+{
+    public float TimeInSeconds;
+
+    private float timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > TimeInSeconds)
+        {
+            timer = 0;
+            GetComponent<SpriteRenderer>().enabled = !GetComponent<SpriteRenderer>().enabled;
+        }
+    }
+}
