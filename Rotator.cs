2fe66641ef7c216dbedc9bf99e672e40599f0d03
Cimeto <84658014+Cimeto@users.noreply.github.com>
2021-05-23 11:43:28 +0200

    Added Rotator.cs

---
+++ Rotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float SpeedX;
+    public float SpeedY;
+    public float SpeedZ;
+
+    private void Update()
+    {
+        transform.Rotate(SpeedX, SpeedY, SpeedZ);
+    }
+}
